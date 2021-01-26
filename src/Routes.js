dcfb6c293196ee0da25fc078b9d7c5d9e3f717ea
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-01-26 15:43:58 -0800

    finished routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,14 @@
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
-import HomePage from './HomePage';
 import NavBar from './NavBar';
+import JobList from './JobList';
+import HomePage from './HomePage';
+import LoginForm from './LoginForm';
+import SignUpForm from './SignUpForm';
+import CompanyList from './CompanyList';
+import ProfileForm from './ProfileForm';
+import CompanyDetails from './CompanyDetails';
+import {Route, BrowserRouter, Switch, Redirect} from 'react-router-dom';
 
-function Routes({currentUser, applyToJob, updateCurrentInfo}) {
+function Routes({currentUser, applyToJob, updateCurrentInfo, createNewUser, loginUser}) {
   return(
       <BrowserRouter>
         <NavBar/>
@@ -10,8 +16,33 @@ function Routes({currentUser, applyToJob, updateCurrentInfo}) {
           <Route exact path="/">
             <HomePage />
           </Route>
+
           <Route exact path="/companies">
             <CompanyList />
+          </Route>
+
+          <Route exact path="/companies/:name">
+            <CompanyDetails applyToJob={applyToJob} />
+          </Route>
+
+          <Route exact path="/jobs">
+            <JobList applyToJob={applyToJob} />
+          </Route>
+
+          <Route exact path="/signup">
+            <SignUpForm handleSubmit={createNewUser} />
+          </Route>
+
+          <Route exact path="/login">
+            <LoginForm handleSubmit={loginUser} />
+          </Route>
+
+          <Route exact path="/profile">
+            <ProfileForm handleSubmit={updateCurrentInfo} user={currentUser} />
+          </Route>
+
+          <Route>
+            <Redirect to="/"/>
           </Route>
         </Switch>
       </BrowserRouter>
