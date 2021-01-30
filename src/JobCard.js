5e33de78acf0bbae8d64b0429002da5552ac5c6b
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-01-29 16:48:46 -0800

    Changed job display

--- src/JobCard.js
+++ src/JobCard.js
@@ -18,11 +18,11 @@ function JobCard({jobId, title, salary, equity, apply}) {
     <div className="w-2/5 mb-4 bg-white shadow overflow-hidden sm:rounded-lg border-solid border-gray-800 rounded-xl shadow-md">
       <div className="px-4 py-5 sm:px-6">
         <h3 className="text-lg leading-6 font-medium text-gray-900">
-          Title:
+          Job Title: {title}
         </h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">
-          {title}
-        </p>
+        {/* <p className="mt-1 max-w-2xl text-sm text-gray-500">
+
+        </p> */}
       </div>
       <div className="border-t border-gray-200">
         <dl>
@@ -31,7 +31,7 @@ function JobCard({jobId, title, salary, equity, apply}) {
               Salary:
             </dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {salary}
+              {salary || "Not Listed"}
             </dd>
           </div>
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -39,7 +39,7 @@ function JobCard({jobId, title, salary, equity, apply}) {
               equity:
             </dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {equity}
+              {equity || 0}
             </dd>
           </div>
         </dl>
